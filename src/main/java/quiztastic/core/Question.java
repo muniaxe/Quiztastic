2af8e49b4ab5c088428f34f233528bbbc8cad71a
Jack <cph-jj484@cphbusiness.dk>
2020-08-27 12:17:01 +0200

    second

--- src/main/java/quiztastic/core/Question.java
+++ src/main/java/quiztastic/core/Question.java
@@ -8,4 +8,25 @@ package quiztastic.core;
 public class Question {
 
     //points, category, question, answer
+
+    private final int score;
+    private final String question;
+    private final String category;
+    private final String answer;
+
+    public Question(int score, String question, String category, String answer){
+        this.score = score;
+        this.question = question;
+        this.category = category;
+
+        this.answer = answer;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
 }
